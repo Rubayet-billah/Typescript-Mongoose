344a71562ad82b7f00adc76f09508f1725b5d35c
Rubayet-billah <rubayetbillah909@gmail.com>
2023-05-18 23:14:46 +0600

    interafce created

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,35 @@ const app: Application = express();
 // use cors
 app.use(cors());
 
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// creating interface
+
+interface IUser {
+  id: string;
+  name: {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+  };
+  role: string;
+  password: string;
+  gender: "male" | "female";
+  email?: string;
+  contactNo: string;
+}
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello World!");
 });
 
 export default app;
+
+/**
+ * create interface
+ * crate schema
+ * create model
+ * database query
+ */
